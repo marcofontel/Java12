c6109327b01db9a1b8c010c8e086dff2f49228f3
server <wll.rgr@gmail.com>
2022-12-04 16:14:10 -0300

    Comparando conteudo de dois arquivos

--- src/org/arca/mismatch/TestMismatch.java
+++ src/org/arca/mismatch/TestMismatch.java
@@ -9,10 +9,16 @@ public class TestMismatch {
 	public static void main(String[] args) {
 		String ex1 = "ex1.txt";
 		String ex2 = "ex2.txt";
-		String texto = "Texto do arquivo 2";
-		
+		String texto = "Texto";
+		File f1 = new File(ex1);
+		File f2 = new File(ex2);
+	
 		try{
+			
 			Files.writeString(new File(ex2).toPath(), texto);
+			
+			long result = Files.mismatch(f1.toPath(),f2.toPath());
+			System.out.println(result);
 			} catch (IOException e) {
 				
 		}
